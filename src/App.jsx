557366d7ee3e7e05b55168040934529e52dcbfa6
Elecosa13 <info@eleco-sa.ch>
2026-04-09 22:44:04 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Employe from './pages/Employe'
 import Chantier from './pages/Chantier'
 import Rapport from './pages/Rapport'
 import Admin from './pages/Admin'
+import Depannage from './pages/Depannage'
 
 function PrivateRoute({ children, role }) {
   const user = JSON.parse(localStorage.getItem('eleco_user') || 'null')
@@ -20,6 +21,7 @@ export default function App() {
       <Route path="/employe" element={<PrivateRoute role="employe"><Employe /></PrivateRoute>} />
       <Route path="/employe/chantier/:id" element={<PrivateRoute role="employe"><Chantier /></PrivateRoute>} />
       <Route path="/employe/rapport/:id" element={<PrivateRoute role="employe"><Rapport /></PrivateRoute>} />
+      <Route path="/employe/depannage" element={<PrivateRoute role="employe"><Depannage /></PrivateRoute>} />
       <Route path="/admin" element={<PrivateRoute role="admin"><Admin /></PrivateRoute>} />
     </Routes>
   )
